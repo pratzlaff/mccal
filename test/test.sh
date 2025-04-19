f5dd5f575c3dff88b49a66da6305d5411ae98249
Pete Ratzlaff <pratzlaff@cfa.harvard.edu>
2025-04-18 20:47:19 -0400

    pick up proper Perl on HEAD network

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+perl=/proj/axaf/bin/perl
+[[ $(hostname) =~ legs|milagro ]] && perl=perl
+
 n=30
 infile=acis_none.arf
 infile_base=`basename $infile | sed 's/\..*//' `
-outdir=./out2
+outdir=./out
 
 rm -rf $outdir
 mkdir -p $outdir
@@ -11,6 +14,6 @@ mkdir -p $outdir
 for i in $(seq 1 $n)
 do
     outfile=`printf "%s/%s_%03d.garf" $outdir $infile_base $i`
-    perl ../bin/arfmunge $infile $outfile --speconly
+    $perl ../bin/arfmunge $infile $outfile #--speconly
 done
 
